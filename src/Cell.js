b685f6a8e1b5299fd3755797280bb5075fe6f202
Peter Kreppein <pmkreppein@gmail.com>
2019-01-23 18:08:33 +0000

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -9,9 +9,16 @@ export default class Cell extends Component {
     }
   }
   
+  handleClick = () => {
+    const newColor = this.props.getNewColor()
+    this.setState ({
+      color: newColor
+    })
+  }
+  
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" onClick={this.handleClick} style={{backgroundColor: this.state.color}}>
       </div>
     )
   }
--- src/Matrix.js
+++ src/Matrix.js
@@ -6,11 +6,23 @@ import ColorSelector from './ColorSelector.js'
 export default class Matrix extends Component {
 
   constructor() {
-    super()
+    super
+     this.state = {
+      selectedColor: '#FFF'
+    }
   }
 
+setNewColor = (newColor) => {
+    this.setState({
+      selectedColor: newColor
+    })
+  }
+
+  getNewColor = () => {this.state.selectedColor}
+
+
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} getNewColor={this.getNewColor} />)
   )
 
   genMatrix = () => (
@@ -21,7 +33,7 @@ export default class Matrix extends Component {
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+         <ColorSelector setNewColor={this.setNewColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
